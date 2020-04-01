e0cf1c9f368f608481f5bf5c38dc0a48a9cb598c
Jonathan Kingston <jonathan@jooped.co.uk>
2020-04-01 01:22:19 +0100

    Add maitree font and fixed homepage sizing.

--- src/header.js
+++ src/header.js
@@ -15,7 +15,7 @@ export function render(props) {
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="style.css">
       <link rel="preload" as="style" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="">
-      <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap">
+      <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&family=Maitree&display=swap">
       <meta name="description" content="${props.title}">
       <link rel="manifest" href="/manifest.webmanifest">
       <link rel="preconnect" href="https://www.google-analytics.com">
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export const details = {
 
 export function render(props) {
   return `
-    <div class="sidebyside">
+    <div class="home sidebyside">
       <section class="intro">
         <h1>Discover &amp; Support Local Independents</h1>
         <p>Deli Direct connects you to local Nottingham food and drink shops. With over 100 listings, find out how to connect with and support local businesses.</p>
