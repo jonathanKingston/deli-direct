912eb0f5b5b036afc7ae4ca9945feec9932ae1b0
Jonathan Kingston <jonathan@jooped.co.uk>
2020-04-03 13:58:27 +0100

    Latest build

--- check.js
+++
@@ -1,5 +0,0 @@
-if (!("fetchs" in window)) {
-  let unsupportedElement = document.createElement("div");
-  unsupportedElement.innerText = "Unfortunately your browser isn't supported yet";
-  document.body.appendChild(unsupportedElement);
-}
